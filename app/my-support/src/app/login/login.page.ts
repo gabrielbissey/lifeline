ac96774125385aae4e52fb0d240d610fe3a59e29
Gabriel Bissey <gabrielbissey@gmail.com>
2019-11-22 18:10:11 -0700

    UPDATE: Add init func login.page

--- app/my-support/src/app/login/login.page.ts
+++ app/my-support/src/app/login/login.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+    loginForm: FormGroup;
 
-  constructor() { }
+    constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.initForm();
+    }
 
+    initForm(): void {
+        this.loginForm = this.fb.group({
+            email: [null, Validators.required],
+            password: [null, Validators.required]
+        });
+    }
+
+    submitForm(form: FormGroup): void {
+        console.log(form.value);
+
+        // should eventually call the http service login method
+        // and skip this class method entirely.
+    }
 }
