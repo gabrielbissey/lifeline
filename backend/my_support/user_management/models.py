9e554affdce50e03c060941f0af5fda402ae7e06
Gabriel Bissey <gabrielbissey@gmail.com>
2019-11-30 16:44:23 -0700

    MODELS: Add init database models

--- backend/my_support/user_management/models.py
+++ backend/my_support/user_management/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from passlib.hash import pbkdf2_sha256 as passlib
+
+class User(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=256)
+    supporter = models.BooleanField()
+    supported_person_id = models.IntegerField(blank=True, default=-1)
+
+    def verify_password(self, raw_password):
+        return passlib.verify(self.password, raw_password)
+
+class Event(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=256)
+    time = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Request(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=256)
+    handling_user = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
