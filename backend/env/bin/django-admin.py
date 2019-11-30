7c3bde36c1f3e683a12ccb7db12228d56066bca7
Gabriel Bissey <gabrielbissey@gmail.com>
2019-11-30 10:50:29 -0700

    INSTALL: Install Django

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pfuufy/Documents/programming/my-support/backend/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
