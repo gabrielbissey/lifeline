1708ce4bdd6131844aca8f57f2c786c4a9458741
Gabriel Bissey <gabrielbissey@gmail.com>
2019-12-12 23:26:30 -0700

    NEW: Install mongo db deps

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const dbCreds = require('./db-creds');
 const app = express();
 const port = 3000;
 
-app.get('/a', (req, res) => res.send('hello'))
+// npx nodemon to run nodemon
+app.use(express.json());
+
+const uri = `mongodb+srv://${dbCreds.name}:${dbCreds.password}@cluster0-wvmfd.mongodb.net/test?retryWrites=true&w=majority`
+MongoClient.connect(uri, function(err, client) {
+    
+    if (err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+    }
+
+    console.log('Connected...');
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.post('/create-account', (req, res) => {
+    console.log(req.body.firstName);
+    res.send(req.body.firstName);
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
