5c1957ac64ab9204cbc174c223197dff4f5150c5
Gabriel Bissey <gabrielbissey@gmail.com>
2020-03-01 22:34:31 -0700

    REFACTOR: tab3.page -> calendar.page

--- app/src/app/tabs/tabs-routing.module.ts
+++ app/src/app/tabs/tabs-routing.module.ts
@@ -28,12 +28,12 @@ const routes: Routes = [
                 ]
             },
             {
-                path: 'tab3',
+                path: 'calendar',
                 children: [
                     {
                         path: '',
                         loadChildren: () =>
-                            import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+                            import('../calendar/calendar.module').then(m => m.CalendarPageModule)
                     }
                 ]
             },
